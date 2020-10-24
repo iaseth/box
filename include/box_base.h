ac245c5728911bd4e6427f26758a5f184cf03ddb
Ankur Seth <iankurseth@gmail.com>
2020-10-25 02:52:25 +0530

    Added prototypes for constructors and destructors

--- include/box_base.h
+++ include/box_base.h
@@ -1,9 +1,16 @@
 #ifndef BOX_BASE_H_IS_INCLUDED
 #define BOX_BASE_H_IS_INCLUDED
 
+#include <stdlib.h>
+
 #include "box_types.h"
 
 
+BoxOwner box_new_owner (char *name);
+BoxOwner box_delete_owner (BoxOwner owner);
+
+Box box_new (BoxOwner owner, size_t size);
+Box box_delete (BoxOwner owner, Box box);
 
 
 
