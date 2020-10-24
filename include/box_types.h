d71c5741fa41035b0de559279689419ba2372fa4
Ankur Seth <iankurseth@gmail.com>
2020-10-25 02:46:40 +0530

    Added structs BoxOwnerStruct and BoxStruct

--- include/box_types.h
+++ include/box_types.h
@@ -2,6 +2,35 @@
 #define BOX_TYPES_H_IS_INCLUDED
 
 
+struct BoxOwnerStruct;
+struct BoxStruct;
+
+typedef struct BoxOwnerStruct BoxOwnerStruct;
+typedef struct BoxStruct BoxStruct;
+
+typedef struct BoxOwnerStruct *BoxOwner;
+typedef struct BoxStruct *Box;
+
+
+struct BoxOwnerStruct {
+	BoxOwner parent;
+	BoxOwner first_child;
+	BoxOwner last_child;
+
+	Box *boxes;
+	int boxes_length;
+
+	int boxes_currently_allocated;
+	int boxes_currently_in_use;
+
+	int bytes_currently_allocated;
+	int bytes_currently_in_use;
+};
+
+struct BoxStruct {
+	void *memory;
+	int size;
+};
 
 
 
