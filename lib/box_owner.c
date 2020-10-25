8c926c6160abd262a2361b0803ebd32ddefcd744
Ankur Seth <iankurseth@gmail.com>
2020-10-25 06:40:40 +0530

    Now initing and freeing slots

--- lib/box_owner.c
+++ lib/box_owner.c
@@ -9,12 +9,15 @@ box_new_owner (char *name)
 {
 	BoxOwner owner = malloc(sizeof(BoxOwnerStruct));
 	strcpy(owner->name, name);
+	owner->slots = malloc(sizeof(Box) * BOX_MIN_NUMBER_OF_SLOTS);
+	owner->slots_length = BOX_MIN_NUMBER_OF_SLOTS;
 	return owner;
 }
 
 BoxOwner
 box_delete_owner (BoxOwner owner)
 {
+	free(owner->slots);
 	free(owner);
 	return NULL;
 }
